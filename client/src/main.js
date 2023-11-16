717b7e8f54c7420cafa97f6b866db718736aee0c
Sainath Poojary <spoojary614@gmail.com>
2023-11-16 19:30:19 +0530

    :sparkles: feat(frontend): add home, viewNotes and writeNote page

--- client/src/main.js
+++ client/src/main.js
@@ -1,29 +1,74 @@
 $(document).ready(async function () {
   const isLogin = await getUser();
 
-  console.log(isLogin);
-
   if (isLogin) {
-    $("#root").load("/src/pages/Home.html");
+    navigate("/");
   } else {
-    $("#root").load("/src/pages/Login.html");
+    navigate("/login");
   }
 });
 
-const navigate = (path) => {
-  console.log("called");
+const navigate = async (path, props) => {
+  console.log("navigating..");
 
   switch (path) {
     case "/":
-      $("#root").load("/src/pages/Home.html");
+      await $("#root").load("/src/pages/Home.html");
+      populateNotes();
       break;
     case "/login":
-      $("#root").load("/src/pages/Login.html");
+      await $("#root").load("/src/pages/Login.html");
       break;
     case "/register":
-      $("#root").load("/src/pages/Register.html");
+      await $("#root").load("/src/pages/Register.html");
+      break;
+    case "/write":
+      await $("#root").load("/src/pages/Write.html");
+      break;
+    case "/view":
+      await $("#root").load("/src/pages/View.html");
+      populateView(props.note_id);
       break;
   }
+};
+
+// utils
+const populateNotes = async () => {
+  const res = await getNotes();
+  console.log(res);
+
+  const cardContainer = $("#cardContainer");
+
+  console.log(cardContainer);
+
+  res.forEach((note) => {
+    console.log(note);
+
+    const card = `<div
+    onclick="navigate('/view',{note_id: '${note.note_id}'})"
+  class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+>
+  <h5
+    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+  >
+    ${note.title}
+  </h5>
+  <p class="font-normal text-gray-700 dark:text-gray-400">
+    ${note.content.slice(0, 100)}
+  </p>
+</div>`;
+    cardContainer.append(card);
+  });
+};
+
+const populateView = async (note_id) => {
+  const res = await getNote(note_id);
+
+  const content = $("#content");
+  const title = $("#title");
+
+  title.text(res.title);
+  content.text(res.content);
 };
 
 // Handlers
@@ -58,17 +103,40 @@ const handleRegister = async (event) => {
   }
 };
 
+const handleSave = async (event) => {
+  event.preventDefault();
+
+  const title = $("#title").val();
+  const content = $("#content").val();
+
+  const res = await createNote(title, content);
+
+  if (res) {
+    navigate("/");
+  } else {
+    alert("Something went wrong");
+  }
+};
+
+const handleLogout = async (event) => {
+  const res = await logout();
+
+  if (res) {
+    navigate("/login");
+  }
+};
+
 // Authentification
 const login = async (email, password) => {
-  var myHeaders = new Headers();
+  let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  var raw = JSON.stringify({
+  let raw = JSON.stringify({
     email: email,
     password: password,
   });
 
-  var requestOptions = {
+  let requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: raw,
@@ -93,16 +161,16 @@ const login = async (email, password) => {
 };
 
 const register = async (email, name, password) => {
-  var myHeaders = new Headers();
+  let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  var raw = JSON.stringify({
+  let raw = JSON.stringify({
     email,
     name,
     password,
   });
 
-  var requestOptions = {
+  let requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: raw,
@@ -125,9 +193,9 @@ const register = async (email, name, password) => {
 };
 
 const logout = async () => {
-  var myHeaders = new Headers();
+  let myHeaders = new Headers();
 
-  var requestOptions = {
+  let requestOptions = {
     method: "GET",
     headers: myHeaders,
     redirect: "follow",
@@ -150,9 +218,9 @@ const logout = async () => {
 
 // User
 const getUser = async () => {
-  var myHeaders = new Headers();
+  let myHeaders = new Headers();
 
-  var requestOptions = {
+  let requestOptions = {
     method: "GET",
     headers: myHeaders,
     redirect: "follow",
@@ -161,6 +229,87 @@ const getUser = async () => {
 
   const response = await fetch(
     "http://localhost:4000/api/v1/user",
+    requestOptions
+  );
+  const data = await response.json();
+
+  if (response.status === 200) {
+    return data;
+  } else {
+    console.log(data);
+    return false;
+  }
+};
+
+// Notes
+const getNotes = async () => {
+  console.log("getting notes");
+  let myHeaders = new Headers();
+
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+    credentials: "include",
+  };
+
+  const response = await fetch(
+    "http://localhost:4000/api/v1/notes",
+    requestOptions
+  );
+  const data = await response.json();
+
+  if (response.status === 200) {
+    return data;
+  } else {
+    console.log(data);
+    return false;
+  }
+};
+
+const createNote = async (title, content) => {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let raw = JSON.stringify({
+    title,
+    content,
+  });
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+    credentials: "include",
+  };
+
+  const response = await fetch(
+    "http://localhost:4000/api/v1/note",
+    requestOptions
+  );
+  const data = await response.json();
+
+  if (response.status === 200) {
+    return data;
+  } else {
+    console.log(data);
+    return false;
+  }
+};
+
+const getNote = async (note_id) => {
+  let myHeaders = new Headers();
+
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+    credentials: "include",
+  };
+
+  const response = await fetch(
+    `http://localhost:4000/api/v1/note/${note_id}`,
     requestOptions
   );
   const data = await response.json();
